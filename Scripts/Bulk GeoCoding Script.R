d6095d9e316b28bee82b368305dca9e207ecdbf2
Christopher Mann <ctmann@gmail.com>
2015-05-25 11:51:48 -0400

    Geocoded Station Addresses, Batch 1_of_3
    Google only allows for the bulk geocoding of 2,500 addresses at a time.
    The 1033 dataset currently has more than 6,000 unique addresses
    (station names), and so must be geocoded in three distinct batches.
    This is the first.

---
+++ Scripts/Bulk GeoCoding Script.R
@@ -0,0 +1,87 @@
+#load up the ggmap library
+library(ggmap)
+library(dplyr)
+
+# Batch 1
+# get the input data; assumes dla is currently in workspace
+# Run "Import and DataCleaning" script, then...
+# break dla addresses into three subgroups...each less than 2,500
+# because Google will only bulk geocode 25000 at a time
+
+# Create Subgroup named addresses
+addresses <- distinct(select(dla, Station.Name))
+
+# alter the Station.Name to address, so Google will recognize it
+colnames(addresses)[1] <- "address"
+addresses
+
+# GeoCode first batch of locations- google only allows 2500 ance
+#Sunday, May 25
+geo1 <- addresses$address[1:2450,]
+geo1 <- geocode(geo1)
+geo1 <- tbl_df(geo1)
+
+# bind address and lat/long together
+geo1.addresses <- addresses[1:2450,]
+geo1.lat_lon <- geo1
+geo1.complete <- bind_cols(geo1.addresses, geo1.lat_lon)
+
+# save to csv, in case R crashes
+write.csv(geo1.complete, "dla.geocoordinates.1of3.csv")
+
+
+
+
+# Batch 2
+# Run "Import and DataCleaning" script, then
+# break dla addresses into three subgroups...each less than 2,500
+# because Google will only bulk geocode 25000 at a time
+
+# Create Subgroup named addresses
+addresses <- distinct(select(dla, Station.Name))
+
+# alter the Station.Name to address, so Google will recognize it
+colnames(addresses)[1] <- "address"
+
+geo2 <- addresses[2451:4900,]
+geo2 <- geocode(geo2$address)
+geo2 <- tbl_df(geo2)
+
+
+# bind address and lat/long together
+geo2.addresses <- addresses[2451:4900,]
+geo2.lat_lon <- geo2
+geo2.complete <- bind_cols(geo2.addresses, geo2.lat_lon)
+
+# save to csv, in case R crashes
+write.csv(geo2.complete, "dla.geocoordinates.2of3.csv")
+
+
+
+# Batch 3
+# Run "Import and DataCleaning" script, then
+# break dla addresses into three subgroups...each less than 2,500
+# because Google will only bulk geocode 25000 at a time
+
+# Create Subgroup named addresses
+addresses <- distinct(select(dla, Station.Name))
+
+# alter the Station.Name to address, so Google will recognize it
+colnames(addresses)[1] <- "address"
+
+geo3 <- addresses[4901:6559]
+geo3 <- geocode(geo3$address)
+geo3 <- tbl_df(geo3)
+
+
+# bind address and lat/long together
+geo3.addresses <- addresses[2451:4900,]
+geo3.lat_lon <- geo3
+geo3.complete <- bind_cols(geo3.addresses, geo3.lat_lon)
+
+# save to csv, in case R crashes
+write.csv(geo3.complete, "dla.geocoordinates.3of3.csv")
+
+
+### End ####
+
