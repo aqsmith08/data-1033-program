f0236bcd471e4104c2499831fb9110c4a829705c
Aaron Smith <aqsmith08@gmail.com>
2015-06-30 20:48:34 -0600

    Tried to load the maps files

--- DataViz/usa_county_dataviz.R
+++ DataViz/usa_county_dataviz.R
@@ -2,23 +2,25 @@ library(dplyr)
 library(ggvis)
 library(ggplot2)
 library(rgdal)
+library(maptools)
+
+# Used this blog post as a guide
+# http://www.r-bloggers.com/making-static-interactive-maps-with-ggvis-using-ggvis-maps-wshiny/
+
+# setwd("~/GitHub/data-1033-program/DataViz"
+usa <- readOGR("us.geojson", "OGRGeoJSON")
+map <- ggplot2::fortify(usa, region="GEOID")
+
+# No idea what any of this means
+usa <- usa[!usa$STATEFP %in% c("02", "15", "72"),]
+us_aea <- spTransform(usa, CRS("+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"))
+
+# Once I tried this, I received an error
+map <- ggplot2::fortify(us_aea, region="GEOID")
+
+# Error: isTRUE(gpclibPermitStatus()) is not TRUE
+# After doing a bunch of researching and trying to install the gpclib package, I gave up.
+# Le sigh.
 
 # setwd("~/GitHub/data-1033-program/DataSets")
 data <- read.csv("MAIN_1033_Transaction Dataset, Updated 06-20-2015, 2020.csv", header = TRUE)
-
-# Followed this blog post
-# http://www.r-bloggers.com/making-static-interactive-maps-with-ggvis-using-ggvis-maps-wshiny/
-
-usa <- readOGR("ggvis-maps/data/us.geojson", "OGRGeoJSON")
-
-maine <- readOGR("data/maine.geojson", "OGRGeoJSON")
-
-map <- ggplot2::fortify(maine, region="name")
-
-map %>%
-  ggvis(~long, ~lat) %>%
-  group_by(group, id) %>%
-  layer_paths(strokeOpacity:=0.5, stroke:="#7f7f7f") %>%
-  hide_legend("fill") %>%
-  hide_axis("x") %>% hide_axis("y") %>%
-  set_options(width=400, height=600, keep_aspect=TRUE)
